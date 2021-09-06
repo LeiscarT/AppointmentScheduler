84bc1d7337ecb15b664943b0dc3e97b8066553fe
LeiscarT <leiscartp29@gmail.com>
2021-09-06 13:02:42 -0400

    add connection string and dbcontext

--- AppointmentScheduler/Startup.cs
+++ AppointmentScheduler/Startup.cs
@@ -1,6 +1,8 @@
+using AppointmentScheduler.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,7 @@ namespace AppointmentScheduler
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
