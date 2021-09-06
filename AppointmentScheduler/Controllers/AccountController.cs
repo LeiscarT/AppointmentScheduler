877023b8b5f64da5a23b45f29f15cd4e226696ba
LeiscarT <leiscartp29@gmail.com>
2021-09-06 13:28:28 -0400

    add styling and js

---
+++ AppointmentScheduler/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using AppointmentScheduler.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduler.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AccountController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
