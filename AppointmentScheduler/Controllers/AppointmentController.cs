cf3e281771963c28405f74710647918ff99cb068
LeiscarT <leiscartp29@gmail.com>
2021-09-15 20:18:24 -0400

    add calendar to main menu

--- AppointmentScheduler/Controllers/AppointmentController.cs
+++ AppointmentScheduler/Controllers/AppointmentController.cs
@@ -22,7 +22,7 @@ namespace AppointmentScheduler.Controllers
 
         public IActionResult Index()
         {
-            _appointmentService.GetDoctorList();
+            ViewBag.DoctorList =  _appointmentService.GetDoctorList();
             return View();
         }
     }   
